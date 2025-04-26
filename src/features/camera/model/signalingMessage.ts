e8c0d9e83bd34a776cceeea4892e35394c0158a5
leona-art <leonagraph@gmail.com>
2025-04-26 22:53:55 +0900

    feat: SignalingMessageの構造を改善し、ICE候補とセッション記述を明確に定義

--- src/features/camera/model/signalingMessage.ts
+++ src/features/camera/model/signalingMessage.ts
@@ -1,14 +1,51 @@
-import {type} from "arktype";
+import { type } from "arktype";
 
-const messageType=type("'offer'|'answer'|'candidate'");
+const RTCIceCandidate = type({
+    candidate: "string",
+    address: "string | null",
+    component: "'rtcp'|'rtp'|null",
+    foundation: "string | null",
+    port: "number | null",
+    priority: "number | null",
+    protocol: "'udp'|'tcp' | null",
+    relatedAddress: "string | null",
+    relatedPort: "number | null",
+    sdpMLineIndex: "number | null",
+    sdpMid: "string | null",
+    tcpType: "'active'|'passive'|'so' | null",
+    type: "'host'|'srflx'|'prflx'|'relay' | null",
+    usernameFragment: "string | null",
+});
 
-export const SignalingMessage=type({
-    type:messageType,
-    payload:"object"
-})
+const RTCSessionDescriptionInit = type({
+    "sdp?": "string",
+    type: "'answer'|'offer'|'pranswer'|'rollback'",
+});
 
-export type SignalingMessageType=typeof SignalingMessage.infer;
 
-export const parseSignalingMessage=type("string")
-    .pipe(s=>JSON.parse(s))
+const CandidateMessage = type({
+    type: "'candidate'",
+    payload: RTCIceCandidate
+});
+
+const OfferMessage = type({
+    type: "'offer'",
+    payload: RTCSessionDescriptionInit
+});
+
+const AnswerMessage = type({
+    type: "'answer'",
+    payload: RTCSessionDescriptionInit
+});
+
+
+
+export const SignalingMessage = CandidateMessage
+    .or(OfferMessage)
+    .or(AnswerMessage)
+
+export type SignalingMessageType = typeof SignalingMessage.infer;
+
+export const parseSignalingMessage = type("string")
+    .pipe(s => JSON.parse(s))
     .to(SignalingMessage)
