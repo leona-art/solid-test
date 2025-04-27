85e9c1e5df1ce4b40b97f14d6681e84327f9f1b3
leona-art <leonagraph@gmail.com>
2025-04-27 23:17:17 +0900

    refactor: useSignalingClientのコードスタイルを改善し、可読性を向上

--- src/features/camera/hooks/useSignaling.ts
+++ src/features/camera/hooks/useSignaling.ts
@@ -3,17 +3,17 @@ import { SignalingMessage, SignalingMessageType } from "../model/signalingMessag
 import { type } from "arktype";
 
 
-export function useSignalingClient(url: string,{
+export function useSignalingClient(url: string, {
     onOffer,
     onAnswer,
     onIceCandidate,
-}:{
-    onOffer:(offer: RTCSessionDescriptionInit)=>void
-    onAnswer:(answer: RTCSessionDescriptionInit)=>void
-    onIceCandidate:(candidate: RTCIceCandidateInit)=>void
+}: {
+    onOffer: (offer: RTCSessionDescriptionInit) => void
+    onAnswer: (answer: RTCSessionDescriptionInit) => void
+    onIceCandidate: (candidate: RTCIceCandidateInit) => void
 }) {
-    const [ws,setWs] = createSignal<WebSocket | null>(null)
-    const [error,setError] = createSignal<string | null>(null)
+    const [ws, setWs] = createSignal<WebSocket | null>(null)
+    const [error, setError] = createSignal<string | null>(null)
 
     const sendMessage = (message: string) => {
         if (ws()) {
@@ -22,98 +22,89 @@ export function useSignalingClient(url: string,{
             console.error("WebSocket is not connected.");
         }
     }
-    const sendOffer=(offer: RTCSessionDescriptionInit) => {
+    const sendOffer = (offer: RTCSessionDescriptionInit) => {
         const websocket = ws();
-        if(!websocket) return;
+        if (!websocket) return;
 
-        const message:SignalingMessageType={
-            type:"offer",
-            payload:offer
-        }
+        const message = SignalingMessage({
+            type: "offer",
+            payload: offer
+        } satisfies SignalingMessageType)
         sendMessage(JSON.stringify(message));
     }
 
-    const sendAnswer=(descriptionInit: RTCSessionDescriptionInit) => {
+    const sendAnswer = (descriptionInit: RTCSessionDescriptionInit) => {
         const websocket = ws();
-        if(!websocket) return;
-        const message:SignalingMessageType={
-            type:"answer",
-            payload:descriptionInit
-        }
+        if (!websocket) return;
+        const message = SignalingMessage({
+            type: "answer",
+            payload: descriptionInit
+        } satisfies SignalingMessageType)
         sendMessage(JSON.stringify(message));
     }
-    const sendIceCandidate=(candidate: RTCIceCandidateInit) => {
+    const sendIceCandidate = (candidate: RTCIceCandidate) => {
         const websocket = ws();
-        if(!websocket) return;
-        const message:SignalingMessageType={
-            type:"candidate",
-            payload:candidate
-        }
+        if (!websocket) return;
+        const message = SignalingMessage({
+            type: "candidate",
+            payload: candidate
+        } satisfies SignalingMessageType)
         sendMessage(JSON.stringify(message));
     }
 
-    onMount(()=>{
+
+    onMount(() => {
         const websocket = new WebSocket(url);
-            setWs(websocket);
+        setWs(websocket);
 
-            websocket.onopen = () => {
-                console.log("WebSocket connection opened");
-            };
+        websocket.onopen = () => {
+            console.log("WebSocket connection opened");
+        };
 
-            websocket.onmessage = (event) => {
-                console.log("Received message:", event.data);
-                try{
-                    
-                    const message=SignalingMessage(event.data)
-                    if(message instanceof type.errors){
-                        console.error("Invalid message format:", message);
-                        return;
-                    }
-                    switch (message.type) {
-                        case "offer":
-                            console.log("Received offer:", message.payload);
-                            onOffer(message.payload);
-                            break;
-                        case "answer":
-                            console.log("Received answer:", message.payload);
-                            onAnswer(message.payload);
-                            break;
-                        case "candidate":
-                            console.log("Received ICE candidate:", message.payload);
-                            onIceCandidate(message.payload);
-                            break;
-                        default:
-                            console.error("Unknown message type:", message.type);
-                            setError("Unknown message type received.");
-                            break;
-                    }
-                } catch (error) {
-                    console.error("Error parsing message:", error);
-                }
-            };
+        websocket.onmessage = (event) => {
+            console.log("Received message:", event.data);
+            const message = SignalingMessage(event.data)
+            if (message instanceof type.errors) {
+                console.error("Invalid message format:", message);
+                return;
+            }
+            switch (message.type) {
+                case "offer":
+                    console.log("Received offer:", message.payload);
+                    onOffer(message.payload);
+                    break;
+                case "answer":
+                    console.log("Received answer:", message.payload);
+                    onAnswer(message.payload);
+                    break;
+                case "candidate":
+                    console.log("Received ICE candidate:", message.payload);
+                    onIceCandidate(message.payload);
+                    break;
+            }
+        };
 
-            websocket.onerror = (event) => {
-                console.error("WebSocket error:", event);
-                setError("WebSocket error occurred.");
-            };
+        websocket.onerror = (event) => {
+            console.error("WebSocket error:", event);
+            setError("WebSocket error occurred.");
+        };
 
-            websocket.onclose = () => {
-                console.log("WebSocket connection closed");
-                setWs(null);
-                setError("WebSocket connection closed.");
-            };
+        websocket.onclose = () => {
+            console.log("WebSocket connection closed");
+            setWs(null);
+            setError("WebSocket connection closed.");
+        };
     });
 
-    onCleanup(()=>{
-        if (ws()) {
-            ws()!.close();
-        }
+    onCleanup(() => {
+        const websocket = ws();
+        if (!websocket) return;
+        websocket.close();
+        console.log("WebSocket connection closed");
         setWs(null);
         setError(null);
     })
     return {
-        sendMessage,
-        error,
         sendOffer,
         sendAnswer,
         sendIceCandidate
